ddf87f5ddd74e8d36c0f4556432f3d94549b2295
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-11-11 23:02:35 +0900

    コマンドライン引数を受け取る

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt" // Goの標準パッケージ(文字の出力や生成に使われる頻出パッケージ)
 )
 
-// プログラム実行時に自動的に呼ばれる関数
 func main() {
-	// fmtパッケージのPrintIn関数で標準出力
-	fmt.Println("Hello world 🍣")
+	flag.Parse()
+	arg := flag.Arg(0) // コマンドライン引数を受け取り変数に入れる
+	fmt.Printf("Hello %s\n", arg)
 }
